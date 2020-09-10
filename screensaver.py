ef3599a7e1ecd32efc4ff452831cbcfac652a7bf
Toni <toni.sredanovic@gmail.com>
2020-09-10 14:49:31 +0200

    off by face

--- screensaver.py
+++ screensaver.py
@@ -3,8 +3,10 @@ from time import sleep
 from datetime import datetime
 
 import pyautogui
+import cv2
 
-N = 10  # Number of seconds the screensaver plays
+OPENCV_FILE_PATH = '/home/toni/Projects/bosch-screensaver/opencv/haarcascade_frontalface_default.xml' # Model for recognizing faces
+N = 30  # Number of seconds the screensaver plays
 
 def stop_screensaver():
     if pyautogui.position() != (1, 1):
@@ -13,15 +15,30 @@ def stop_screensaver():
         pyautogui.moveTo(20, 20)
 
 
-def stop_screensaver_after_n():
+def stop_screensaver_after_n_or_face():
     started_at = datetime.now()
+    cap = cv2.VideoCapture(0)
     while True:
+        # Check if the screensaver is on
         if not screensaver_is_on():
+            cap.release()
             return 'EXTERNAL', (datetime.now() - started_at).total_seconds()
+        
+        # Check if time out
         seconds_on = (datetime.now() - started_at).total_seconds()
         if seconds_on > N:
             stop_screensaver()
+            cap.release()
             return 'TIMEOUT', (datetime.now() - started_at).total_seconds()
+        
+        # Check for face
+        _, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        if len(faces):
+            stop_screensaver()
+            cap.release()
+            return 'FACE', (datetime.now() - started_at).total_seconds()
 
 
 def screensaver_is_on():
@@ -33,15 +50,10 @@ def screensaver_is_on():
 # If the screensaver is off, do nothing
 # If the screensaver is on, turn it off after N second
 
+face_cascade = cv2.CascadeClassifier(OPENCV_FILE_PATH)
+
 while True:
     if screensaver_is_on():
         print('{} - Screensaver ON'.format(datetime.now()))
-        stopped_by, seconds_on = stop_screensaver_after_n()
+        stopped_by, seconds_on = stop_screensaver_after_n_or_face()
         print('{} - Screensaver OFF by {}, was on for {} seconds'.format(datetime.now(), stopped_by, seconds_on))
-
-
-started_at = datetime.now()
-sleep(75)
-current_time = datetime.now()
-import pdb
-pdb.set_trace()
